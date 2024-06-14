9c2ad1f0b21b95d7901da22206c2d73b8c318a60
Abdul-Azeez Adeleye <133106484+Abdul-AzeezGFA@users.noreply.github.com>
2024-06-14 14:54:05 +0100

    Updated Template Section

--- src/dimp-pages/customer-training/TemplatesNoPlay.jsx
+++ src/dimp-pages/customer-training/TemplatesNoPlay.jsx
@@ -18,8 +18,8 @@ const TemplatesNoPlay = () => {
             <Row className="align-items-center">
               <Col lg={12} md={12} xs={12}>
                 <div className="mb-5 mb-lg-0">
-                  <h2 className="display-5 fw-bold mb-3 text-center">Show the utmost care for your students with<br/> an awesome branded academy</h2>
-                  <p className="display-8 text-center">Choose one of 50+ DIMP's website templates, customize it with a few clicks, upload <br/>your courses, and you are good to grow. So simple, so powerful!
+                  <h2 className="display-5 fw-bold mb-3 text-center">Prioritise the well-being of your learners by offering a <br/>fantastic branded learning experience.</h2>
+                  <p className="display-8 text-center">Select from a variety of DIMP's website templates, customize it with few clicks,<br/> upload your course and you’re good to go. So simple, so powerful.
 </p> 
                   
                 </div>
