f9187b444fb9810706ac3e4059682fed5b42d827
Bolaji <bolajistephen72@gmail.com>
2024-07-12 00:26:45 +0100

    integrate myuser endpiont

--- src/Layout/AllRoutes.jsx
+++ src/Layout/AllRoutes.jsx
@@ -111,6 +111,7 @@ import EcosystemSignUp from "../EcosystemDashboard/Authentication/EcosystemSignU
 import EcosystemForgetPassword from "../EcosystemDashboard/Authentication/EcosystemForgetPassword";
 import EcosystemEmailVerification from "../EcosystemDashboard/Authentication/EcosystemEmailVerification";
 import EcosystemVerifyEmail from "../EcosystemDashboard/Authentication/EcosystemVerifyEmail";
+import DeveloperProgram from "../developer-program/DeveloperProgram";
 
 const AllRoutes = () => {
   return (
