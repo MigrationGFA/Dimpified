2b69219cefa99aebb0cf1e44f8f868761fc56e8b
Samuel-Makinde <makindesamuel1999@gmail.com>
2024-08-12 14:10:43 +0100

    commit changes

--- src/data/Template/LandingPageTemplate.js
+++ src/data/Template/LandingPageTemplate.js
@@ -1,19 +1,79 @@
 import template1 from "../../assets/template/templateNew1.png";
 import template2 from "../../assets/template/templateNew2.png";
-
+ 
 const Templates = [
   {
     id: 1,
     name: "Template 1",
     img: template1,
-    sectionId: 8, // Government
+   //sectionId: 8,
   },
   {
     id: 2,
-    name: "Template 2",
-    img: template2,
-    sectionId: 4, // Personal Care Services
+    name: "Template 1",
+    img: template1,
+   //sectionId: 8,
   },
+  {
+    id: 3,
+    name: "Template 1",
+    img: template1,
+   //sectionId: 8,
+  },{
+    id: 4,
+    name: "Template 1",
+    img: template1,
+   //sectionId: 8,
+  },
+  {
+    id: 5,
+    name: "Template 1",
+    img: template1,
+   //sectionId: 8,
+  },
+  {
+    id: 6,
+    name: "Template 1",
+    img: template1,
+   //sectionId: 8,
+  },
+  {
+    id: 7,
+    name: "Template 1",
+    img: template1,
+   //sectionId: 8,
+  },
+  {
+    id: 8,
+    name: "Template 1",
+    img: template1,
+   //sectionId: 8,
+  },
+  {
+    id: 9,
+    name: "Template 1",
+    img: template1,
+   //sectionId: 8,
+  },
+  {
+    id: 10,
+    name: "Template 1",
+    img: template1,
+   //sectionId: 8,
+  },
+  {
+    id: 11,
+    name: "Template 1",
+    img: template1,
+   //sectionId: 8,
+  },
+ 
+  // {
+  //   id: 2,
+  //   name: "Template 2",
+  //   img: template2,
+  //   sectionId: 4,
+  // },
 ];
-
+ 
 export default Templates;
